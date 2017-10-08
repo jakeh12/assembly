21a3bc7d6f3aa7cf08fc279d8529feb3e4631c21
jhladik <jhladik@iastate.edu>
2017-10-08 16:47:29 -0500

    Cleaned up asm file, made it work with new zasm

--- Makefile
+++ Makefile
@@ -1,18 +1,13 @@
 TARGET=counter
 PROM=AT28C256
-SIZE=32768
-
 
 all:
-	zasm -b -i $(TARGET).asm -o $(TARGET).bin
+	zasm -buy -i $(TARGET).asm -o $(TARGET).bin
 clean:
-	rm -f *.bin
+	rm -f *.bin *.lst
 flash:
-	mkfile -n $(SIZE) $(TARGET)_flash.bin
-	dd if=$(TARGET).bin of=$(TARGET)_flash.bin bs=1 conv=notrunc
-	minipro -p $(PROM) -w $(TARGET)_flash.bin
+	minipro -p $(PROM) -w $(TARGET).bin
 	wc -c $(TARGET).bin
-	hexdump $(TARGET)_flash.bin
-	rm -f *_flash.bin
+	hexdump $(TARGET).bin
 verify:
 	test
--- counter.asm
+++ counter.asm
@@ -1,36 +1,49 @@
-	;; compile with
-	;; make
-	;; and flash with
-	;; make flash
-	;;
+;; this program uses RAM (0xFFF) for a simple counter demo
+;; compile with
+;; make
+;; and flash with
+;; make flash
+;;
+	org $0000
+reset:
+	di
+	jp init
+
+	org $0100
+init:
 	;; put 0 into a
 	ld a,$00
 	;; put address FFFF into hl
 	ld hl,$FFFF
 	;; write a into (HL)
 	ld (hl), a
-start:
+main:
 	;; read (HL) into a
 	ld a,(hl)
 	;; output a
 	out ($00),a
+	
 	;; delay loop start
 	ld c,$ff
 loopon1:
 	ld d,$ff
 loopon2:
 	dec d
-  	jp nz,loopon2
-
-  	dec c
-  	jp nz,loopon1
+	jp nz,loopon2
+	
+	dec c
+	jp nz,loopon1
 	;; delay loop end
-
+	
 	;; increment a
-  	inc a
-  	;; save it back into (HL)
-  	ld (hl), a
-  	;; write zero into a to make sure RAM is working
-  	ld a,$00
-  	;; goto start
-  	jp start
+	inc a
+	;; save it back into (HL)
+	ld (hl), a
+	;; write zero into a to make sure RAM is working
+	ld a,$00
+	;; goto start
+	jp main
+
+	;; pad file to eeprom size
+	ds	0x8000 - $
+
